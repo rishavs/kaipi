e871ba921d4cb9684d1f96aa54a0653717309071
Rishav Sharan <rishav.sharan@live.com>
2018-06-20 18:32:31 +0530

    fixed the knex env issue

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ require('dotenv').config({path: ''})
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'find some way of adding the postgres string here....',
+    connection: process.env.DATABASE_URL,
     migrations: {
         tableName: 'knex_migrations'
     },
