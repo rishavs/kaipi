f19cf6b59a24e74c740fb1b39313b8ad1584845f
Rishav Sharan <rishav.sharan@live.com>
2021-05-24 12:30:15 +0530

    merged api and web routes

--- src/handlers.cr
+++ src/handlers.cr
@@ -1,17 +1,22 @@
-module Kaipi::Handlers
+module Kaipi
 
-    class ContentTypeHandler < Kemal::Handler
-        def call(env)
-            env.response.content_type = "application/json"
-            call_next(env)
+    class APIvsWebHandler
+        include HTTP::Handler
+      
+        def call(ctx)
+            if ctx.request.path.starts_with?("/api/v1/")
+                ctx.response.content_type                                   = "application/json"
+                ctx.response.headers["Access-Control-Request-Headers"]      = "Content-Type, application/json"
+                ctx.response.headers["Access-Control-Allow-Origin"]         = "http://127.0.0.1:8080"
+                ctx.response.headers["Access-Control-Allow-Credentials"]    = "true"
+                ctx.response.headers["Access-Control-Allow-Methods"]        = "POST, GET, OPTIONS"
+                ctx.response.headers["Access-Control-Allow-Content-Type"]   = "application/json"
+                ctx.response.headers["Access-Control-Allow-Headers"]        = "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+            else
+                ctx.response.content_type = "text/html; charset=UTF-8"
+            end
+            call_next(ctx)
         end
-    end
-
-    class CorsHandler < Kemal::Handler
-        def call(env)
-            env.response.headers["Access-Control-Allow-Origin"] = "*"
-            call_next(env)
-        end    
     end
 
 end
--- src/kaipi.cr
+++ src/kaipi.cr
@@ -4,12 +4,13 @@ require "pg"
 require "http/server"
 require "ecr"
 
+require "./handlers.cr"
 require "./views/*"
 # require "./errors.cr"
 # require "./models.cr"
 # require "./actions/*"
 require "./routes.cr"
-# require "./handlers.cr"
+
 
 module Kaipi
     VERSION = "0.1.0"
@@ -27,14 +28,14 @@ module Kaipi
     # puts "Connected to DB at: #{cnn_time}"
 
     server = HTTP::Server.new([
+        APIvsWebHandler.new,
         HTTP::ErrorHandler.new,
-        HTTP::LogHandler.new,
+        # HTTP::LogHandler.new,
         HTTP::CompressHandler.new,
-        HTTP::StaticFileHandler.new(public_dir = "./public", fallthrough = true, directory_listing = false),
+        # HTTP::StaticFileHandler.new(public_dir = "./public", fallthrough = true, directory_listing = false),
     ]) do |ctx|
-        ctx.response.content_type = "text/html; charset=UTF-8"
-
-        url_parts = ctx.request.path.split("/", limit: 2, remove_empty: true)
+        pp! ctx.request.path
+        url_parts = ctx.request.path.split('/', limit: 4, remove_empty: true)
 
         pp! url_resource =     url_parts[0]?
         pp! url_identifier =   url_parts[1]?
@@ -43,27 +44,42 @@ module Kaipi
         # Render shared views and components
         navbar = Navbar.render()
 
-        case {url_resource, url_identifier, url_verb}
+        case {ctx.request.method, url_resource, url_identifier, url_verb}
 
-        when {"about", nil, nil}
+        # -----------------------------------------
+        # API Routes
+        # -----------------------------------------
+        when {"GET", "api", "v1", "dosomething"}
+            ctx.response.print "Hello world!"
+            # if ctx.request.query_params["id"]? && ctx.request.query_params["name"]?
+            #     ctx.response.respond_with_status(200, "Signing up - " + ctx.request.query_params["id"] + ", " + ctx.request.query_params["name"])
+            # else
+            #     ctx.response.respond_with_status(400, "Missing id?")
+            # end
+
+        # -----------------------------------------
+        # Web Routes
+        # -----------------------------------------
+        when {"GET", "about", nil, nil}
             sidebar = Sidebar.render()
             # sidebar = ECR.render "src/views/components/sidebar.ecr"
             page = About.render(ctx)
             view = Layout.render(navbar, page, sidebar)
+            ctx.response.print view
 
         # Catch-all routes    
-        when {nil, nil, nil}
+        when {"GET", nil, nil, nil}
             sidebar = Sidebar.render()
             page = Home.render(ctx)
             view = Layout.render(navbar, page, sidebar)
+            ctx.response.print view
+
         else
             sidebar = nil
             page = Error.render(404)
             view = Layout.render(navbar, page, sidebar)
+            ctx.response.print view
         end
-
-        ctx.response.print view
-
 
     end
 
